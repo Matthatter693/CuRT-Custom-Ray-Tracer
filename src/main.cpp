6a158190d9dc950279507a77d35c7d6ff2c1df53
Matthatter693 <pra060903@gmail.com>
2025-07-13 12:37:21 +0530

    New commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,21 @@
 
 #include<iostream>
 
-/*color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);  // t ranges from 0 (bottom) to 1 (top)
-
-    // Interpolate between white (1,1,1) and blue (0,0,1)
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.0, 0.0, 1.0);
-}*/
-
+bool hit_sphere(const point3 &center,double radius,const ray& r)
+{
+    vec3 oc=center-r.origin();
+    auto a=dot(r.direction(),r.direction());
+    auto b=-2.0*dot(r.direction(),oc);
+    auto c=dot(oc,oc)-radius*radius;
+    auto discriminant = b*b-4*a*c;
+    return (discriminant>=0);
+}
 color ray_color(const ray& r)
 {
     //return color(0,0,0);
+    if(hit_sphere(point3(0,0,-1),0.5,r))
+        return color(1,0,0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a=0.5*(unit_direction.y()+1.0);
     return(1.0-a)*color(1.0,1.0,1.0)+a*color(0.5,0.7,1.0);
